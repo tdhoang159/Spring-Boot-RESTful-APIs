7c5fa64a138606c4d6df65543efb9f494dd53222
Truong Duc Hoang <tantiennguyen2404@gmail.com>
2026-03-31 16:41:18 +0700

    First commit

---
+++ src/main/java/com/truongduchoang/SpringBootRESTfullAPIs/SpringBootResTfullApIsApplication.java
@@ -0,0 +1,13 @@
+package com.truongduchoang.SpringBootRESTfullAPIs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootResTfullApIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootResTfullApIsApplication.class, args);
+	}
+
+}
